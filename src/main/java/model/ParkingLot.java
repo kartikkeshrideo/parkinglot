03a8928d67854ea79a93259dfb350a094c3d3148
kartikkeshrideo <koolkartik.94@gmail.com>
2019-08-07 00:47:00 +0530

    added model classes

---
+++ src/main/java/model/ParkingLot.java
@@ -0,0 +1,50 @@
+package model;
+
+import java.util.Map;
+import java.util.PriorityQueue;
+
+public class ParkingLot {
+    private int capacity;
+    private Map<Integer, Car> carsParked = new HashMap<>();
+    private PriorityQueue<Integer> availableSlots = new PriorityQueue<>();
+
+    public ParkingLot(int capacity) {
+        this.capacity = capacity;
+        for(int i=0;i<capacity;i++) {
+            availableSlots.add(i+1);
+        }
+    }
+
+    public boolean isFull() {
+        return availableSlots.isEmpty();
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public Map<Integer, Car> getCarsParked() {
+        return carsParked;
+    }
+
+    public void setCarsParked(Map<Integer, Car> carsParked) {
+        this.carsParked = carsParked;
+    }
+
+    public PriorityQueue<Integer> getAvailableSlots() {
+        return availableSlots;
+    }
+
+    public void setAvailableSlots(PriorityQueue<Integer> availableSlots) {
+        this.availableSlots = availableSlots;
+    }
+
+    public int getAvailableSlotNumber() {
+        return availableSlots.peek();
+    }
+
+}
---
+++ src/main/java/model/ParkingSpot.java
@@ -0,0 +1,17 @@
+package model;
+
+public class ParkingSpot {
+    Integer slotNo;
+
+    public ParkingSpot(Integer slotNo) {
+        this.slotNo = slotNo;
+    }
+
+    public Integer getSlotNo() {
+        return slotNo;
+    }
+
+    public void setSlotNo(Integer slotNo) {
+        this.slotNo = slotNo;
+    }
+}
